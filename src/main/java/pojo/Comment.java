ce82feb0b100bec3230d5adfb32de9ea1b87ac98
tx3012 <2629583847@qq.com>
2023-09-30 20:41:26 +0800

    add pojo

---
+++ src/main/java/pojo/Comment.java
@@ -0,0 +1,47 @@
+package pojo;
+
+// 数据库表条目对象
+//mysql构建:
+//create table comment (
+//        comment_id int not null primary key auto_increment,
+//        comment_name varchar(20) not null,
+//        comment_goods int not null,
+//        comment_tags json not null,
+//        comment_text varchar(1024) not null
+//        );
+
+import com.alibaba.fastjson.JSON;
+
+public class Comment {
+    protected Integer commentID;
+    protected String commentName;
+    protected Integer commentGoods;
+    protected JSON commentTags;
+    protected String commentText;
+
+    public Comment(Integer commentID, String commentName, Integer commentGoods, JSON commentTags, String commentText) {
+        this.commentID = commentID;
+        this.commentName = commentName;
+        this.commentGoods = commentGoods;
+        this.commentTags = commentTags;
+        this.commentText = commentText;
+    }
+
+    public Comment(String commentName, Integer commentGoods, JSON commentTags, String commentText) {
+        this.commentName = commentName;
+        this.commentGoods = commentGoods;
+        this.commentTags = commentTags;
+        this.commentText = commentText;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "commentID=" + commentID +
+                ", commentName='" + commentName + '\'' +
+                ", commentGoods=" + commentGoods +
+                ", commentTags=" + commentTags.toString() +
+                ", commentText='" + commentText + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/pojo/Orders.java
@@ -0,0 +1,67 @@
+package pojo;
+
+// 数据库表条目对象
+//mysql构建:
+//create table orders (
+//        order_id int not null primary key auto_increment,
+//        order_orderer varchar(20) not null,
+//        order_store varchar(20) not null,
+//        order_list json not null,
+//        order_total_price varchar(8) not null,
+//        order_type int not null,
+//        order_time datetime not null,
+//        order_status int not null
+//        );
+
+import com.alibaba.fastjson.JSON;
+import org.joda.time.DateTime;
+
+public class Orders {
+    protected Integer orderID;
+    protected String orderOrderer;
+    protected String orderStore;
+    protected JSON orderList;
+    protected String orderType;
+    protected DateTime orderTime;
+    protected Integer orderStatus;
+
+    public Orders(Integer orderID, String orderOrderer, String orderStore, JSON orderList, String orderType, DateTime orderTime, Integer orderStatus) {
+        this.orderID = orderID;
+        this.orderOrderer = orderOrderer;
+        this.orderStore = orderStore;
+        this.orderList = orderList;
+        this.orderType = orderType;
+        this.orderTime = orderTime;
+        this.orderStatus = orderStatus;
+    }
+
+    public Orders(String orderOrderer, String orderStore, JSON orderList, String orderType, DateTime orderTime, Integer orderStatus) {
+        this.orderOrderer = orderOrderer;
+        this.orderStore = orderStore;
+        this.orderList = orderList;
+        this.orderType = orderType;
+        this.orderTime = orderTime;
+        this.orderStatus = orderStatus;
+    }
+
+    public Orders(String orderOrderer, String orderStore, JSON orderList, String orderType, DateTime orderTime) {
+        this.orderOrderer = orderOrderer;
+        this.orderStore = orderStore;
+        this.orderList = orderList;
+        this.orderType = orderType;
+        this.orderTime = orderTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "orderID=" + orderID +
+                ", orderOrderer='" + orderOrderer + '\'' +
+                ", orderStore='" + orderStore + '\'' +
+                ", orderList=" + orderList.toString() +
+                ", orderType='" + orderType + '\'' +
+                ", orderTime=" + orderTime +
+                ", orderStatus=" + orderStatus +
+                '}';
+    }
+}
